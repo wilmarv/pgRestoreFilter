7345f64f4db9634e80695d5799692d3ba065e93b
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2024-08-14 19:05:10 -0300

    Adiciona exlusão por loja

--- index.ts
+++ index.ts
@@ -5,11 +5,14 @@ async function main() {
 
     try {
         const inputFilePath = path.resolve("contents.txt");
-        const outputFilePath = path.resolve("selectedSchema.txt");
+        const outputFilePath = path.resolve("/home/compels/Downloads/selectedSchema.txt");
 
         const fileContent = await fs.promises.readFile(inputFilePath, 'utf-8');
         const lines = fileContent.split('\n');
         const modifiedLines: string[] = [];
+
+        const mrPattern = /_mr\d{2}/;
+        const excludedMr = ["_mr01", "_mr02", "_mr04", "_mr06", "_mr18"];
 
         for (let line of lines) {
             if (
@@ -17,13 +20,18 @@ async function main() {
                 line.includes(" bling ") ||
                 line.includes(" ctb ") ||
                 line.includes(" cpdv ") ||
-                line.includes(" kikker ") ||
-                line.includes(" fin ")||
-                line.includes(" imobilizado ")||
-                line.includes(" imp ")||
-                line.includes(" impl ")||
-                line.includes(" messaging ")||
-                line.includes(" sfl ")
+                line.includes(" fin ") ||
+                line.includes(" imobilizado ") ||
+                line.includes(" imp ") ||
+                line.includes(" impl ") ||
+                line.includes(" messaging ") ||
+                line.includes(" lfi ") ||
+                line.includes(" tray ") ||
+                line.includes(" transp ") ||
+                line.includes(" sfl ") ||
+                line.includes("old") ||
+                line.includes("kikker") ||
+                (mrPattern.test(line) && !excludedMr.some(mr => line.includes(mr)))
             ) {
                 line = ";" + line;
             }
